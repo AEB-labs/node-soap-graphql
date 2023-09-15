aed8188925415cbeff14547a105bbcb46142ab35
Peter Schroeder <peter.schroeder@aeb.com>
2023-09-15 08:45:50 +0200

    fix specs

--- spec/src/soap2graphql/name-resolver.spec.ts
+++ spec/src/soap2graphql/name-resolver.spec.ts
@@ -14,51 +14,63 @@ describe('name-resolver', () => {
     it('defaultOutputNameResolver', async () => {
         expect(defaultOutputNameResolver(null)).to.not.exist;
         expect(defaultOutputNameResolver(undefined)).to.not.exist;
-        expect(defaultOutputNameResolver({ name: null } as any)).to.not.exist;
-        expect(defaultOutputNameResolver({ name: undefined } as any)).to.not.exist;
-        expect(defaultOutputNameResolver({ name: '' } as any)).to.not.exist;
+        expect(defaultOutputNameResolver({ name: null, namespace: 'N' } as any)).to.not.exist;
+        expect(defaultOutputNameResolver({ name: undefined, namespace: 'N' } as any)).to.not.exist;
+        expect(defaultOutputNameResolver({ name: '', namespace: 'N' } as any)).to.not.exist;
 
-        expect(defaultOutputNameResolver({ name: 'a' } as any)).to.equal('A');
-        expect(defaultOutputNameResolver({ name: 'A' } as any)).to.equal('A');
+        expect(defaultOutputNameResolver({ name: 'a', namespace: '' } as any)).to.equal('_A');
+        expect(defaultOutputNameResolver({ name: 'A', namespace: '' } as any)).to.equal('_A');
 
-        expect(defaultOutputNameResolver({ name: 'ab' } as any)).to.equal('Ab');
-        expect(defaultOutputNameResolver({ name: 'Ab' } as any)).to.equal('Ab');
+        expect(defaultOutputNameResolver({ name: 'a', namespace: 'n' } as any)).to.equal('N_A');
+        expect(defaultOutputNameResolver({ name: 'A', namespace: 'N' } as any)).to.equal('N_A');
 
-        expect(defaultOutputNameResolver({ name: 'abc' } as any)).to.equal('Abc');
-        expect(defaultOutputNameResolver({ name: 'Abc' } as any)).to.equal('Abc');
+        expect(defaultOutputNameResolver({ name: 'ab', namespace: 'N' } as any)).to.equal('N_Ab');
+        expect(defaultOutputNameResolver({ name: 'Ab', namespace: 'N' } as any)).to.equal('N_Ab');
+
+        expect(defaultOutputNameResolver({ name: 'abc', namespace: 'N' } as any)).to.equal('N_Abc');
+        expect(defaultOutputNameResolver({ name: 'Abc', namespace: 'N' } as any)).to.equal('N_Abc');
     }).timeout(10000);
 
     it('defaultInterfaceNameResolver', async () => {
         expect(defaultInterfaceNameResolver(null)).to.not.exist;
         expect(defaultInterfaceNameResolver(undefined)).to.not.exist;
-        expect(defaultInterfaceNameResolver({ name: null } as any)).to.not.exist;
-        expect(defaultInterfaceNameResolver({ name: undefined } as any)).to.not.exist;
-        expect(defaultOutputNameResolver({ name: '' } as any)).to.not.exist;
+        expect(defaultInterfaceNameResolver({ name: null, namespace: 'N' } as any)).to.not.exist;
+        expect(defaultInterfaceNameResolver({ name: undefined, namespace: 'N' } as any)).to.not
+            .exist;
+        expect(defaultOutputNameResolver({ name: '', namespace: 'N' } as any)).to.not.exist;
 
-        expect(defaultInterfaceNameResolver({ name: 'a' } as any)).to.equal('iA');
-        expect(defaultInterfaceNameResolver({ name: 'A' } as any)).to.equal('iA');
+        expect(defaultInterfaceNameResolver({ name: 'a', namespace: 'N' } as any)).to.equal('iA');
+        expect(defaultInterfaceNameResolver({ name: 'A', namespace: 'N' } as any)).to.equal('iA');
 
-        expect(defaultInterfaceNameResolver({ name: 'ab' } as any)).to.equal('iAb');
-        expect(defaultInterfaceNameResolver({ name: 'Ab' } as any)).to.equal('iAb');
+        expect(defaultInterfaceNameResolver({ name: 'ab', namespace: 'N' } as any)).to.equal('iAb');
+        expect(defaultInterfaceNameResolver({ name: 'Ab', namespace: 'N' } as any)).to.equal('iAb');
 
-        expect(defaultInterfaceNameResolver({ name: 'abc' } as any)).to.equal('iAbc');
-        expect(defaultInterfaceNameResolver({ name: 'Abc' } as any)).to.equal('iAbc');
+        expect(defaultInterfaceNameResolver({ name: 'abc', namespace: 'N' } as any)).to.equal(
+            'iAbc',
+        );
+        expect(defaultInterfaceNameResolver({ name: 'Abc', namespace: 'N' } as any)).to.equal(
+            'iAbc',
+        );
     }).timeout(10000);
 
     it('defaultInputNameResolver', async () => {
         expect(defaultInputNameResolver(null)).to.not.exist;
         expect(defaultInputNameResolver(undefined)).to.not.exist;
-        expect(defaultInputNameResolver({ name: null } as any)).to.not.exist;
-        expect(defaultInputNameResolver({ name: undefined } as any)).to.not.exist;
-        expect(defaultInputNameResolver({ name: '' } as any)).to.not.exist;
+        expect(defaultInputNameResolver({ name: null, namespace: 'N' } as any)).to.not.exist;
+        expect(defaultInputNameResolver({ name: undefined, namespace: 'N' } as any)).to.not.exist;
+        expect(defaultInputNameResolver({ name: '', namespace: 'N' } as any)).to.not.exist;
 
-        expect(defaultInputNameResolver({ name: 'a' } as any)).to.equal('AInput');
-        expect(defaultInputNameResolver({ name: 'A' } as any)).to.equal('AInput');
+        expect(defaultInputNameResolver({ name: 'a', namespace: 'N' } as any)).to.equal('N_AInput');
+        expect(defaultInputNameResolver({ name: 'A', namespace: 'N' } as any)).to.equal('N_AInput');
 
-        expect(defaultInputNameResolver({ name: 'ab' } as any)).to.equal('AbInput');
-        expect(defaultInputNameResolver({ name: 'Ab' } as any)).to.equal('AbInput');
+        expect(defaultInputNameResolver({ name: 'ab', namespace: 'N' } as any)).to.equal('N_AbInput');
+        expect(defaultInputNameResolver({ name: 'Ab', namespace: 'N' } as any)).to.equal('N_AbInput');
 
-        expect(defaultInputNameResolver({ name: 'abc' } as any)).to.equal('AbcInput');
-        expect(defaultInputNameResolver({ name: 'Abc' } as any)).to.equal('AbcInput');
+        expect(defaultInputNameResolver({ name: 'abc', namespace: 'N' } as any)).to.equal(
+            'N_AbcInput',
+        );
+        expect(defaultInputNameResolver({ name: 'Abc', namespace: 'N' } as any)).to.equal(
+            'N_AbcInput',
+        );
     }).timeout(10000);
 });
